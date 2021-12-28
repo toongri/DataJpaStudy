ee7429fe075bfe1f289c7aa042aa08db61dad117
Toong_laptop <jhkim03284@gmail.com>
2021-12-28 23:57:38 +0900

    7-1th commit : test

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    @Override
+    void flush();
 }
